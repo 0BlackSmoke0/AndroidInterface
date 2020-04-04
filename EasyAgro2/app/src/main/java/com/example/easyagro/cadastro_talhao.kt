c25ce8e09774bf689d30b02224b7b556239f4ca8
Paulo Bertucci <bertucci.henrique@gmail.com>
2020-04-03 23:04:58 -0300

    app EasyAgro

---
+++ EasyAgro2/app/src/main/java/com/example/easyagro/cadastro_talhao.kt
@@ -0,0 +1,14 @@
+
+
+//package com.example.easyagro
+
+//import android.support.v7.app.AppCompatActivity
+//import android.os.Bundle
+
+//class cadastro_talhao : AppCompatActivity() {
+
+    //override fun onCreate(savedInstanceState: Bundle?) {
+      //  super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_cadastro_talhao)
+    //}
+//}
---
+++ EasyAgro2/app/src/main/java/com/example/easyagro/cadastro_usuario.kt
@@ -0,0 +1,12 @@
+//package com.example.easyagro
+//
+//import android.support.v7.app.AppCompatActivity
+//import android.os.Bundle
+//
+//class cadastro_usuario : AppCompatActivity() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_cadastro_usuario)
+//    }
+//}
